7761cbc82779b2f5a93196522a2b8e6d7b894acd
Madhu-baloni <madhu.baloni@celestialsys.com>
2024-11-26 17:28:21 +0530

    start redux toolkit

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,31 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 const Counter = () =>  {
-    const counter = useSelector(state => state.counter)
+    const counter = useSelector(state => state.counter);
+    const show = useSelector(state => state.showCounter)
+    const dispatch = useDispatch();
+
+    const handleIncrement = () => {
+        dispatch({type:"increment"})
+    }
+    const handleDecrement = () => {
+        dispatch({type:"decrement"})
+    }
+    const handleIncrease = () => {
+      dispatch({type:"increase", value:5})
+    }
+
 
    const handlerToggle = () => {
-      
+      dispatch({type:'toggle'})
    };
 return (
     <>
     <main>
         <h1>Redux Counter</h1>
         <div>{counter}</div>
+        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleIncrease}>Increase By 5</button>
+        <button onClick={handleDecrement}>Decrement</button>
         <button onClick={handlerToggle}>Toggle Me</button>
     </main>
     </>
